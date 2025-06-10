fa020e0869c8b02dd53ee93b6df804d710642065
thierry-is-cool <thierrychan@connect.kellettschool.com>
2025-06-10 03:52:45 +0000

    I have completed the slow print

--- test.py
+++ test.py
@@ -1,6 +1,4 @@
 from colorama import Fore, Back, Style
-print(Fore.RED + 'some red text')
-print(Back.GREEN + 'and with a green background')
-print(Style.DIM + 'and in dim text')
+print(Fore.BLUE + 'some red text')
 print(Style.RESET_ALL)
 print('back to normal now')
