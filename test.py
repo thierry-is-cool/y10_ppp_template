303da64b21115aa79188d5270f1a547942b5b695
thierry-is-cool <thierrychan@connect.kellettschool.com>
2025-06-13 01:24:17 +0000

    Jun 13, 2025, 1:24 AM

--- test.py
+++ test.py
@@ -1,8 +1,25 @@
 import time
 from colorama import Fore, init, Style
 
-init()
+def print_with_colour(deck):
+    joe = []
+    for i in deck:
+        if i[0] == "B":
+            i = Fore.BLUE + Style.BRIGHT + i + Style.RESET_ALL
+            joe.append(i)
+        elif i[0] == "Y":
+            i = Fore.YELLOW + Style.BRIGHT + i + Style.RESET_ALL
+            joe.append(i)
+        elif i[0] == "R":
+            i = Fore.RED + Style.BRIGHT + i + Style.RESET_ALL
+            joe.append(i)
+        elif i[0] == "G":
+            i = Fore.GREEN + Style.BRIGHT + i + Style.RESET_ALL
+            joe.append(i)
+    
+    return joe
 
-for i in range(101):
-    print(Style.BRIGHT + Fore.CYAN + f"\rProgress: {i}%", end="")
-    time.sleep(0.1)
+deck = ["B1", "Y3", "G4", "R5"]
+deck = print_with_colour(deck)
+
+print(deck)
