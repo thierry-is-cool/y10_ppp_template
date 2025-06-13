b66ac9cd4e50f0cb5867e1b700059f200bad2b57
thierry-is-cool <thierrychan@connect.kellettschool.com>
2025-06-13 01:29:11 +0000

    Jun 13, 2025, 1:29 AM

--- main.py
+++ main.py
@@ -19,7 +19,24 @@ def print_slow(txt):
         print(x, end='', flush = True)  
         sleep(0.025)
     return None
+def print_with_colour(deck):
+    deck1 = []
+    for i in deck:
+        if i[0] == "B":
+            i = Fore.BLUE + Style.BRIGHT + i + Style.RESET_ALL
+            deck1.append(i)
+        elif i[0] == "Y":
+            i = Fore.YELLOW + Style.BRIGHT + i + Style.RESET_ALL
+            deck1.append(i)
+        elif i[0] == "R":
+            i = Fore.RED + Style.BRIGHT + i + Style.RESET_ALL
+            deck1.append(i)
+        elif i[0] == "G":
+            i = Fore.GREEN + Style.BRIGHT + i + Style.RESET_ALL
+            deck1.append(i)
 
+    for i in range(len(deck1)):
+        print_slow(deck1[i], end = " ")
 def print_with_colour(txt):
     pass
 
--- test.py
+++ test.py
@@ -2,24 +2,26 @@ import time
 from colorama import Fore, init, Style
 
 def print_with_colour(deck):
-    joe = []
+    deck1 = []
     for i in deck:
         if i[0] == "B":
             i = Fore.BLUE + Style.BRIGHT + i + Style.RESET_ALL
-            joe.append(i)
+            deck1.append(i)
         elif i[0] == "Y":
             i = Fore.YELLOW + Style.BRIGHT + i + Style.RESET_ALL
-            joe.append(i)
+            deck1.append(i)
         elif i[0] == "R":
             i = Fore.RED + Style.BRIGHT + i + Style.RESET_ALL
-            joe.append(i)
+            deck1.append(i)
         elif i[0] == "G":
             i = Fore.GREEN + Style.BRIGHT + i + Style.RESET_ALL
-            joe.append(i)
-    
-    print(joe)
-    return joe
+            deck1.append(i)
+
+    for i in range(len(deck1)):
+        print(deck1[i], end = " ")
 
 deck = ["B1", "Y3", "G4", "R5"]
 deck = print_with_colour(deck)
 
+for i in range(len(deck)):
+    print(deck[i], end = " ")
